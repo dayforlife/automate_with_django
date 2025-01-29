94c0ff4c04f29fd4b466f0a9ea873807491baa33
dayforlife <eldiyar_alymbekov@icloud.com>
2025-01-29 16:59:51 +0300

    create customer model

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Student
+from .models import Customer, Student
 # Register your models here.
+
+
 admin.site.register(Student)
+admin.site.register(Customer)
